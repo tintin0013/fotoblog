4d0aa4516c10c0bb4ba3cfb2d4e7de9ab82938df
Tintin0013 <arnaudpinatel@arnauds-MacBook-Pro.local>
2024-10-08 10:58:58 +0200

    modification des vues basées sur fonction -> classes

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from . import forms
 
 from django.contrib.auth import login, authenticate, logout
+from django.views.generic import View
 
 
 def logout_user(request):
@@ -9,12 +10,17 @@ def logout_user(request):
     logout(request)
     return redirect('login')
 
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
 
-def login_page(request):
-    form = forms.LoginForm()
-    message = ''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+        
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             user = authenticate(
                 username=form.cleaned_data['username'],
@@ -23,7 +29,24 @@ def login_page(request):
             if user is not None:
                 login(request, user)
                 return redirect('home')
-            else:
-                message = 'Identifiants invalides.'
-    return render(
-        request, 'authentication/login.html', context={'form': form, 'message': message})
+        message = 'Identifiants invalides.'
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
+
+# def login_page(request):
+#     form = forms.LoginForm()
+#     message = ''
+#     if request.method == 'POST':
+#         form = forms.LoginForm(request.POST)
+#         if form.is_valid():
+#             user = authenticate(
+#                 username=form.cleaned_data['username'],
+#                 password=form.cleaned_data['password'],
+#             )
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('home')
+#             else:
+#                 message = 'Identifiants invalides.'
+#     return render(
+#         request, 'authentication/login.html', context={'form': form, 'message': message})
